25d2ee40669f44f2d85a97c7bdb0b5b296c4c941
Menho <82595532+Menho91@users.noreply.github.com>
2025-01-02 17:40:40 +0900

    도전 기능 구현 완료, 벡터 안의 요소 확인 위한 printVector 메서드 추가

--- Homework3/SimpleVector.cpp
+++ Homework3/SimpleVector.cpp
@@ -9,31 +9,82 @@ private:
 	int currentSize;
 	int currentCapacity;
 public:
+	void printVector()
+	{
+		cout << "current size : " << currentSize << ", current capacity : " << currentCapacity << endl;
+		cout << "{ ";
+		for (int i = 0; i < currentSize; i++)
+		{
+			cout << data[i] << " ";
+		}
+		cout << "}" << endl;
+	}
 	SimpleVector(int capacity = 10) : currentSize(0), currentCapacity(capacity)
 	{
 		data = new T[capacity];    // 배열 동적 할당
 	}
-	void resize(int newCapacity) {}
+	SimpleVector(const SimpleVector& other) : currentSize(other.size()), currentCapacity(other.capacity())
+	{
+		data = new T[other.capacity()];
+		for (int i = 0; i < other.size(); i++)
+		{
+			data[i] = other.data[i];
+		}
+	}
+	void resize(int newCapacity)
+	{
+		if (newCapacity > currentCapacity)
+		{
+			currentCapacity = newCapacity;
+			T* newData = new T[currentCapacity];
+			for (int i = 0; i < currentSize; i++)
+			{
+				newData[i] = data[i];
+			}
+			delete[] data;
+			data = newData;
+		}
+	}
 	void push_back(const T& value)  // 인자로 받은 원소를 맨 뒤에 추가
 	{
 		if (currentSize == currentCapacity) {
-			return;
+			resize(currentCapacity + 5);
 		}
-		data[currentSize] = value;
-		currentSize++;
+		data[currentSize++] = value;
 	}
 	void pop_back()  // 벡터의 마지막 원소 제거
 	{
 		if (currentSize != 0)
 		{
-			data[currentSize] = NULL;
 			currentSize--;
 		}
 	}
 	int size() { return currentSize; }
 	int capacity() { return currentCapacity; }
+	void sortData()
+	{
+		data.sort();
+	}
 	~SimpleVector()
 	{
 		delete[] data;  // 동적 할당 메모리 해제
 	}
 };
+
+int main()
+{
+	SimpleVector<int> sv(3);
+	sv.push_back(10);
+	sv.push_back(20);
+	sv.push_back(30);
+	sv.printVector();
+	sv.pop_back();
+	sv.printVector();
+
+	sv.push_back(40);
+	sv.push_back(50);
+	sv.printVector();
+	sv.pop_back();
+	sv.printVector();
+	return 0;
+}
